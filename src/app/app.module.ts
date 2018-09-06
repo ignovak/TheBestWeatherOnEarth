8a0d86ad4dd6bb41843a727c6de77a0f621bce4f
Igor Novak <bezengi@gmail.com>
2018-09-06 08:18:21 +0200

    Implement WeatherService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WeatherTableComponent } from './weather-table/weather-table.component';
@@ -10,7 +11,8 @@ import { WeatherTableComponent } from './weather-table/weather-table.component';
     WeatherTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/weather.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  public getData(): Observable<any> {
+    // https://openweathermap.org/current
+    const url = 'https://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&appid=4ee3ac3ce2b2461555028c24473caa74';
+
+    return this.http.get(url);
+  }
+}
