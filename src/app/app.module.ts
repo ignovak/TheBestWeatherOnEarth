30842b41c5af3b2a8e264b8f1bedbc8ead9f2a50
Igor Novak <bezengi@gmail.com>
2018-09-06 07:45:33 +0200

    Add bootstrap and ng-bootstrap; display mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherTableComponent } from './weather-table/weather-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherTableComponent
   ],
   imports: [
     BrowserModule
