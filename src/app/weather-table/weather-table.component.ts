65e4756fc9b89c63a1824c5cb5df31475eba1f29
Igor Novak <bezengi@gmail.com>
2018-09-06 08:48:25 +0200

    Use cards to display the items

--- src/app/weather-table/weather-table.component.ts
+++ src/app/weather-table/weather-table.component.ts
@@ -6,33 +6,36 @@ import { WeatherService } from '../weather.service';
   template: `
     <ngb-progressbar *ngIf="isLoading" type="info" [striped]="true">
     </ngb-progressbar>
-    <table *ngIf="!isLoading" class="table">
-      <tr *ngFor="let row of data">
-        <td>{{row.id}}</td>
-        <td>{{row.name}}</td>
-        <td>{{row.coord.Lon + ' ' + row.coord.Lat}}</td>
-        <td>{{row.main}}</td>
-        <td>{{row.dt}}</td>
-        <td>{{row.wind}}</td>
-        <td>{{row.clouds}}</td>
-        <td>{{row.rain}}</td>
-        <td>{{row.snow}}</td>
-        <td>{{row.weather}}</td>
-      </tr>
-    </table>
+
+    <div *ngIf="!isLoading">
+      <div *ngFor="let item of data" class="card">
+        <div class="card-header">{{item.name}}</div>
+        <div class="card-body">
+          <h5 class="card-title">{{item.weather[0].main}}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{item.weather[0].description}}</h6>
+          <img [src]="'https://openweathermap.org/img/w/' + item.weather[0].icon + '.png'" />
+          <h6>Temperature: {{item.main.temp}}</h6>
+          <h6>Humidity: {{item.main.humidity}}</h6>
+          <p class="card-text">
+              {{item.data}}
+          </p>
+        </div>
+      </div>
+    </div>
   `,
   styles: []
 })
 export class WeatherTableComponent implements OnInit {
   private data = [];
   private isLoading: false;
+  private MAX_RESULTS_NUM = 20;
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
     this.isLoading = true;
     this.weatherService.getData().subscribe(data => {
-      this.data = data.list;
+      this.data = data.list.slice(0, this.MAX_RESULTS_NUM).map(_ => ({ ..._, data: JSON.stringify(_)}));
       this.isLoading = false;
     });
   }
